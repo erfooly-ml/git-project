9dd87e6ee856aee8b1d9bb4fb13851a36a93b51c
Erfooly <erka29ml@gmil.com>
2026-02-13 03:34:43 -0500

    test2 : define salary fnuc

--- test2.py
+++ test2.py
@@ -1,5 +1,12 @@
 class Emp:
-    def __init__(self,id):
+    increase_rate = 0.05
+    def __init__(self,id, salary):
         self.id = id
+        self.salary= salary
+
     def show_info(self):
         return self.id
+
+    def increase_salary(self):
+        self.salary *= Emp.increase_rate
+
