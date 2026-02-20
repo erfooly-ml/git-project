8eba14c01b5ec4ed4f7a9d868c60818c62cfb78b
Erfooly <erka29ml@gmil.com>
2026-02-20 08:07:56 -0500

    melika update login

--- login.py
+++ login.py
@@ -5,3 +5,7 @@ if id in users.key:
     print(f'welcome {id}')
 else:
     print('sorry, you have to sign in first')
+
+# hi iam melika and i going to add this change :
+for id in users :
+    print (i.keys)
