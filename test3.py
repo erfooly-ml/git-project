6adbffd6dc7f33a99aff52e4eff84700bfb69e57
erfooly-ml <erka29ml@gmail.com>
2026-02-20 12:01:19 +0330

    add print_info: test3py

--- test3.py
+++ test3.py
@@ -2,3 +2,6 @@ from datetime import date
 def show_age(year):
     return date.date - year
 
+# hi, iam melika and i'll add this changes
+def print_info(name):
+    print(f'hey i am  {name} and my age is {show_age(1995)}')
