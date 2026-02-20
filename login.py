1f073832403157bb7875bb04eee17fdc62c2eb21
erfan <erka29ml@gmail.com>
2026-02-20 16:20:34 +0330

    add show result to login page

--- login.py
+++ login.py
@@ -1,2 +1,7 @@
+users = {}
 id = input('enter your username')
 password = input('enter your password')
+if id in users.key:
+    print(f'welcome {id}')
+else:
+    print('sorry, you have to sign in first')
