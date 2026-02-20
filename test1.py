75085ea6956aafeb8c32afaa1bb19af353702270
Erfooly <erka29ml@gmil.com>
2026-02-20 08:27:56 -0500

    melika add class user: test1

--- test1.py
+++ test1.py
@@ -3,4 +3,8 @@ class Person:
         self.name = 'erfan'
     def show_info(self):
         print(self.name)
-#comment for test amend . actually i just commit a file but i had to commit two file . one is escaped and i want to fix this 
+#comment for test amend . actually i just commit a file but i had to commit two file . one is escaped and i want to fix this
+#hi im melika and this change is for me 
+class user:
+    def __init__(self):
+        self.name = 'melika'
